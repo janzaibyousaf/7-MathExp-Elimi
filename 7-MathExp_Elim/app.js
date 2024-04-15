9c7ada7d25799092c6d42a36f87c73f06f9ea025
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-04-16 00:21:30 +0500

    Add files via upload

---
+++ 7-MathExp_Elim/app.js
@@ -0,0 +1,22 @@
+var totalcost = 1 + 3 * 4;
+
+alert(totalcost);
+
+var totalcost = 1 + (3 * 4);
+
+alert(totalcost);
+
+
+var totalcost = (1 + 3) * 4;
+
+alert(totalcost);
+
+
+var computer = ((2 * 4 ) * 4) + 2;
+
+alert(computer);
+
+
+var multiple = (2 * 4 ) * (4 + 2);
+
+alert(multiple);
